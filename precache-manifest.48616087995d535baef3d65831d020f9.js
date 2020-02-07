6a36e7cd18c12f89b216fdd2a5a185ae057e53f8
taiyi <god830311@gmail.com>
2020-02-07 14:52:32 +0800

    Updates

---
+++ precache-manifest.48616087995d535baef3d65831d020f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78489b0dcc240ccd2cda33056de43da8",
+    "url": "/pharmacy-mask-map/index.html"
+  },
+  {
+    "revision": "8920f5d4cd6d60f7ce7b",
+    "url": "/pharmacy-mask-map/static/css/main.8e346b56.chunk.css"
+  },
+  {
+    "revision": "750059aaf5d18fb9d172",
+    "url": "/pharmacy-mask-map/static/js/2.3e7c9c66.chunk.js"
+  },
+  {
+    "revision": "6e498969aafde287c0e2e1a721426fda",
+    "url": "/pharmacy-mask-map/static/js/2.3e7c9c66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8920f5d4cd6d60f7ce7b",
+    "url": "/pharmacy-mask-map/static/js/main.7053ffba.chunk.js"
+  },
+  {
+    "revision": "d8b2372355c4f412ea65",
+    "url": "/pharmacy-mask-map/static/js/runtime-main.b7906378.js"
+  }
+]);
